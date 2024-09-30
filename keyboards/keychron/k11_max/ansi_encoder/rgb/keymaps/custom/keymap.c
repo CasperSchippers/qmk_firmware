18d305a51a235593ad9c5a83b0f242af997fe383
Casper Schippers <Casperschippers@hotmail.com>
2024-09-30 22:05:58 +0200

    Fix incorrect numpad

--- keyboards/keychron/k11_max/ansi_encoder/rgb/keymaps/custom/keymap.c
+++ keyboards/keychron/k11_max/ansi_encoder/rgb/keymaps/custom/keymap.c
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [FN2] = LAYOUT_69_ansi(
         _______, XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_P7,   KC_P8,   KC_P9,   KC_P0,    KC_PMNS,  KC_PPLS,  _______,          _______,
-        _______, XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_P6,   KC_P7,   KC_P6,   XXXXXXX,  KC_PSLS,  KC_PAST,  XXXXXXX,          _______,
+        _______, XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_P4,   KC_P5,   KC_P6,   XXXXXXX,  KC_PSLS,  KC_PAST,  XXXXXXX,          _______,
         _______, XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,           XXXXXXX, KC_P1,   KC_P2,   KC_P3,    XXXXXXX,  XXXXXXX,  _______,          QK_MAKE,
         _______,           XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, KC_PCMM,  KC_PDOT,  XXXXXXX,  _______, QK_BOOT,
         _______, _______,  _______,           _______,          _______,  _______,          KC_P0,             _______,            DB_TOGG, QK_RBT,  EE_CLR ),
